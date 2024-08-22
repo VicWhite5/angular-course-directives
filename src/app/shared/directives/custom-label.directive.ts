158902f2331faa88e2ecab4af0bb887f9e4efa8d
Victor White <victorblanco281201@gmail.com>
2024-08-22 11:40:20 -0400

    Section 23 Done

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,20 +1,71 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
 })
-export class CustomLabelDirective implements OnInit{
-
+export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
+
+  private _color: string = 'red';
+  private _errors?: ValidationErrors | null | undefined;
+
+  @Input() set color(value: string) {
+    this._color = value;
+    this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessages();
+    // console.log(this._errors);
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
-    
-    this.htmlElement.nativeElement.innerHTML = 'Hola Mundo';
   }
 
   ngOnInit(): void {
-    console.log('OnInit de la directiva');
-    
+    // console.log('OnInit de la directiva');
+    this.setStyle();
+  }
+
+  setStyle(): void {
+    if (!this.htmlElement) return;
+
+    this.htmlElement.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessages(): void {
+    if (!this.htmlElement) return;
+
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+    console.log(this._errors);
+
+
+    const errors = Object.keys(this._errors);
+
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+
+    if (errors.includes('minlength')) {
+      const requiredLength = this._errors['minlength']?.requiredLength;
+      this.htmlElement.nativeElement.innerText = requiredLength
+        ? `La longitud mínima es de ${requiredLength} caracteres`
+        : 'Aún no tiene los caracteres necesarios';
+
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerText =
+        'El valor ingresado no tiene formato de email';
+      return;
+    }
   }
 }
